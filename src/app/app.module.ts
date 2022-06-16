a8bdd1acd9afd9775dfdd5ffb60dba8f85a6c734
Antucha <antony.devcoder2199@gmail.com>
2022-06-15 20:47:06 -0500

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+// MATERIAL
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MatButtonModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
